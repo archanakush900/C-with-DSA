85ec1766b24080a7d89e548f2664ae45736ce6b8
Archana <archana65maurya@gmail.com>
2022-12-05 18:55:19 +0530

    my first commit

---
+++ L1.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  int n;
+  cin >> n;
+  if(n>0){
+    cout<<"n is positive"<<endl;
+
+  }
+  else {
+    cout<< "n is negative"<<endl;
+  }
+}
